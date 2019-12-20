9d4e1a312c331e950a5ae829dd36397fe334d012
jensen <shenjian26@hotmail.com>
2019-12-20 16:34:34 +0800

    runtime

--- relife-runtime/src/main/java/com/bzkj/runtime/RuntimeApplication.java
+++ relife-runtime/src/main/java/com/bzkj/runtime/RuntimeApplication.java
@@ -2,6 +2,8 @@ package com.bzkj.runtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @description: Runtime启动类
@@ -9,10 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: created by 2019/9/11 1:36
  */
 @SpringBootApplication
-public class RuntimeApplication {
+public class RunTimeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(RuntimeApplication.class, args);
+        SpringApplication.run(RunTimeApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
