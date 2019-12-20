977903f16b69c541839a96b7bdc3a1a2a5714f13
jensen <shenjian26@hotmail.com>
2019-12-20 16:23:20 +0800

    runtime

--- relife-runtime/src/main/java/com/bzkj/runtime/RunTimeApplication.java
+++ relife-runtime/src/main/java/com/bzkj/runtime/RunTimeApplication.java
@@ -1,1 +1,18 @@
-package com.bzkj.runtime;import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.annotation.ComponentScan;/** * @description: 启动类 * @author: jensen * @date: created by 2019/9/11 1:36 */@SpringBootApplicationpublic class RunTimeApplication {    public static void main(String[] args) {        SpringApplication.run(RunTimeApplication.class, args);    }}
+package com.bzkj.runtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: Runtime启动类
+ * @author: jensen
+ * @date: created by 2019/9/11 1:36
+ */
+@SpringBootApplication
+public class RunTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunTimeApplication.class, args);
+    }
+
+}
