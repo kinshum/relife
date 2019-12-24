7c990adce332ce2aec4bbe87d1b7e7043215e622
jensen <shenjian26@hotmail.com>
2019-12-24 17:04:17 +0800

    sql

--- relife-auth/src/main/java/com/bzkj/auth/controller/schedule/QuartzController.java
+++ relife-auth/src/main/java/com/bzkj/auth/controller/schedule/QuartzController.java
@@ -3,30 +3,30 @@ package com.bzkj.auth.controller.schedule;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.bzkj.auth.controller.base.BaseController;
-import com.bzkj.entity.HttpJobLogs;
 import com.bzkj.entity.Page;
 import com.bzkj.entity.param.AddHttpJobParam;
 import com.bzkj.entity.vo.HttpJobDetailVO;
 import com.bzkj.facade.schedule.HttpJobService;
 import com.bzkj.facade.schedule.JobManageService;
-import com.bzkj.rsp.Response;
+import com.bzkj.rsp.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description: Http类型任务Controller
+ * @description: quartz
  * @author: jensen
  * @date: created by 2019/9/15 2:09
  */
-@Api(tags = "httpJob")
+@Api(tags = "quartz")
 @RestController
-@RequestMapping(value = "/api/v1/httpJob")
-public class HttpJobController extends BaseController {
+@RequestMapping(value = "/api/v1/quartz")
+public class QuartzController extends BaseController {
 
     @Reference
     private HttpJobService httpJobService;
@@ -45,8 +45,8 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "params", value = "参数"),
             @ApiImplicitParam(paramType = "query", required = true, name = "cronExpression", value = "cron表达式")
     })
-    public Response addPostJsonJob(String jobName, String jobGroup, String description,
-                                   String requestType, String url, String params, String cronExpression) {
+    public Result addPostJsonJob(String jobName, String jobGroup, String description,
+                                 String requestType, String url, String params, String cronExpression) {
         AddHttpJobParam addHttpJobParam = new AddHttpJobParam();
         addHttpJobParam.setCronExpression(cronExpression);
         addHttpJobParam.setDescription(description);
@@ -56,7 +56,7 @@ public class HttpJobController extends BaseController {
         addHttpJobParam.setRequestType(requestType);
         addHttpJobParam.setUrl(url);
         httpJobService.addHttpJob(addHttpJobParam);
-        return Response.success();
+        return Result.newSuccessResult();
     }
 
 
@@ -67,9 +67,9 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "pageNum", defaultValue = "1", value = "页码")
     })
     @GetMapping(value = "/jobs")
-    public Response<Page<HttpJobDetailVO>> getJobs(String searchParam, Integer pageSize, Integer pageNum) {
+    public Result<Page<HttpJobDetailVO>> getJobs(String searchParam, Integer pageSize, Integer pageNum) {
         Page<HttpJobDetailVO> result = httpJobService.getHttpJobs(searchParam, pageSize, pageNum);
-        return Response.success(result);
+        return Result.newSuccessResult(result);
     }
 
 
@@ -79,9 +79,9 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "jobGroup", value = "任务组")
     })
     @PostMapping("/delete")
-    public Response deleteJob(String jobName, String jobGroup) {
+    public Result deleteJob(String jobName, String jobGroup) {
         jobManageService.deleteJob(jobName, jobGroup);
-        return Response.success();
+        return Result.newSuccessResult();
     }
 
     @ApiOperation("job任务恢复")
@@ -90,9 +90,9 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "jobName", value = "任务名称"),
             @ApiImplicitParam(paramType = "query", required = true, name = "jobGroup", value = "任务组")
     })
-    public Response resumeJob(String jobName, String jobGroup) {
+    public Result resumeJob(String jobName, String jobGroup) {
         jobManageService.resumeJob(jobName, jobGroup);
-        return Response.success();
+        return Result.newSuccessResult();
     }
 
     @ApiOperation("job任务暂停")
@@ -101,9 +101,9 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "jobName", value = "任务名称"),
             @ApiImplicitParam(paramType = "query", required = true, name = "jobGroup", value = "任务组")
     })
-    public Response pauseJob(String jobName, String jobGroup) {
+    public Result pauseJob(String jobName, String jobGroup) {
         jobManageService.pauseJob(jobName, jobGroup);
-        return Response.success();
+        return Result.newSuccessResult();
     }
 
     @ApiOperation("job任务更新")
@@ -113,32 +113,9 @@ public class HttpJobController extends BaseController {
             @ApiImplicitParam(paramType = "query", required = true, name = "cronExpression", value = "cron表达式")
     })
     @PostMapping("/update")
-    public Response updateJob(String jobName, String jobGroup, String cronExpression) {
+    public Result updateJob(String jobName, String jobGroup, String cronExpression) {
         jobManageService.updateCronExpression(jobName, jobGroup, cronExpression);
-        return Response.success();
+        return Result.newSuccessResult();
     }
-
-
-   /* @RequestMapping(value = "/historyJobs")
-    public Response<Page<HttpJobDetailVO>> getHistoryJobs(@RequestParam(name = "searchParam", required = false) String searchParam,
-                                                          @RequestParam(name = "pageSize", required = false, defaultValue = "15") Integer pageSize,
-                                                          @RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-
-        Page<HttpJobDetailVO> result = httpJobService.getHistoryHttpJobs(searchParam, pageSize, pageNum);
-        return Response.success(result);
-
-    }
-
-    @RequestMapping(value = "/jobLogs")
-    public Response<Page<HttpJobLogs>> getJobLogs(@RequestParam(name = "jobName", required = false) String jobName,
-                                                  @RequestParam(name = "jobGroup", required = false) String jobGroup,
-                                                  @RequestParam(name = "searchParam", required = false) String searchParam,
-                                                  @RequestParam(name = "pageSize", required = false, defaultValue = "15") Integer pageSize,
-                                                  @RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-
-        Page<HttpJobLogs> result = httpJobService.getHttpJobLogs(jobName, jobGroup, searchParam, pageSize, pageNum);
-        return Response.success(result);
-    }*/
-
 
 }
